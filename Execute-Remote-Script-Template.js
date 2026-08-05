6b36ce63e86f370353c1c9b33d0fd932ae7c20c0
TrashMaker3000 <alexcahill02@outlook.com>
2026-08-04 17:48:39 -0700

    Updated ERST

--- Execute-Remote-Script-Template.js
+++ Execute-Remote-Script-Template.js
@@ -1,6 +1,18 @@
 
+// Script Settings
 
-const ScriptUrl = "";
+const CacheValidDuration = 60 * 24; // In Minutes, or 0 to disable;
+const ScriptUrl = "https://raw.githubusercontent.com/alexcahill/iOS-Scriptable-Remote-Scripts/main/TestScript.js";
 
+// Cache Logic
 
+if (!Keychain.contains(Script.name()+".Cache.Date") || (new Date()).getTime() >= Keychain.get(Script.name()+".Cache.Date") + CacheValidDuration * 60000 /* To MS */) {
 
+    var Req = new Request(ScriptUrl);
+    var Contents = await Req.loadString();
+    Keychain.set(Script.name()+".Cache.Value", Contents);
+    Keychain.set(Script.name()+".Cache.Date", (new Date()).getTime());
+
+}    
+
+eval(Keychain.get(Script.name()+".Cache.Value"));
