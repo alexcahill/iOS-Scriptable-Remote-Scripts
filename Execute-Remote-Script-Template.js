d2bce42e0720e6aa94ea5e79e534906435ac1701
TrashMaker3000 <alexcahill02@outlook.com>
2026-08-04 18:32:45 -0700

    Bug Fix
    Added try catch for offline Request attempts.

--- Execute-Remote-Script-Template.js
+++ Execute-Remote-Script-Template.js
@@ -10,10 +10,20 @@ if (CacheValidDuration == 0 ||
     !Keychain.contains(Script.name()+".Cache.Date") || 
     (new Date()).getTime() >= parseInt(Keychain.get(Script.name()+".Cache.Date")) + CacheValidDuration * 60000 /* To MS */) {
 
-    var Req = new Request(ScriptUrl);
-    var Contents = await Req.loadString();
-    Keychain.set(Script.name()+".Cache.Value", Contents);
-    Keychain.set(Script.name()+".Cache.Date", (new Date()).getTime().toString());
+    try {
+        var Req = new Request(ScriptUrl);
+        var Contents = await Req.loadString();
+        Keychain.set(Script.name()+".Cache.Value", Contents);
+        Keychain.set(Script.name()+".Cache.Date", (new Date()).getTime().toString());
+    } catch (Error) {
+
+        // Revert to last Cache...
+        console.log("Error, Reverting to last Cache...");
+        console.log(Error);
+        if (!Keychain.contains(Script.name()+".Cache.Value"))
+            throw "Could not Get Script Content, and no Cache exists...";
+
+    }
 
 }    
 
