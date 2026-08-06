d5ce63d5611e0d8ece0834eee4484db8be5b94a8
TrashMaker3000 <alexcahill02@outlook.com>
2026-08-06 01:52:27 -0700

    Update WiseBank - FX Monitor.js

--- Public Projects/WiseBank - FX Monitor.js
+++ Public Projects/WiseBank - FX Monitor.js
@@ -14,11 +14,29 @@ var Parameters = {
     Fees: 0
 };
 
-/*
-async function GetCurrentBid() {
-    return 0;
+
+async function GetCurrentBid() { // In Yen
+
+    var Req = new Request("https://query1.finance.yahoo.com/v6/finance/quote/marketSummary");
+    var Result = await Req.loadJSON();
+
+    if (!Result['marketSummaryResponse'])
+        throw "Error, unexpected response";
+    Result = Result['marketSummaryResponse'];
+    if (!Result['result'] || Result['result'] == 0)
+        throw "Error, no result";
+    Result = Result['result'];
+    for (var i = 0; i < Result.length; i++)
+        if (Result[i]['currency'] && Result[i]['currency'] == "JPY")
+            return Result[i]['regularMarketPrice']['raw'];
+    throw "Error, JPY not in the result list";
+    
 }
 
+
+console.log(await GetCurrentBid());
+
+/*
 
 function floor(Number, Places = 2) {
 
